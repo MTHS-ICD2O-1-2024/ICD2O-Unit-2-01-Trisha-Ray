ca689af78469864940e551c5a4f9e4a0fc33735c
default <trisha.ray@stu.ocsb.ca>
2025-03-05 17:58:46 +0000

    title

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Trisha Ray
-// Created on: March 4 2025
+// Created on: March 5 2025
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
